98d083384f3c3d99aa0ee5e3cdc60b1ddddc5611
physidev <physidev@users.noreply.github.com>
2019-10-31 17:25:11 -0400

    Pretty

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function init() {
 			this.classList.toggle("active");
 			
 			var panel = this.nextElementSibling;
-			if(panel.style.display === "block") {
-				panel.style.display = "none";
+			if (panel.style.maxHeight) {
+			  panel.style.maxHeight = null;
 			} else {
-				panel.style.display = "block";
-			}
+			  panel.style.maxHeight = panel.scrollHeight + "px";
+			} 
 		});
 	}
 }
