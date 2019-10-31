dc1d3e20528969f9a3fe35b92b03a6b7d0061143
physidev <physidev@users.noreply.github.com>
2019-10-31 16:56:11 -0400

    Add search

---
+++ script.js
@@ -0,0 +1,32 @@
+function init() {
+	var button = document.getElementsByClassName("button");
+	
+	for(let i=0; i < button.length; i++) {
+		button[i].addEventListener("click", function() {
+			this.classList.toggle("active");
+			
+			var panel = this.nextElementSibling;
+			if(panel.style.display === "block") {
+				panel.style.display = "none";
+			} else {
+				panel.style.display = "block";
+			}
+		});
+	}
+}
+
+function search() {
+	var searchBar, searchText, sections;
+	searchBar = document.getElementById("search");
+	searchText = searchBar.value.toUpperCase();
+	sections = document.getElementById("accordion").getElementsByClassName("section");
+	
+	for(let i=0; i < sections.length; i++) {
+		let section = sections[i].getElementsByClassName("button")[0];
+		if(section.innerHTML.toUpperCase().indexOf(searchText) > -1) {
+			sections[i].style.display = "block";
+		} else {
+			sections[i].style.display = "none";
+		}
+	}
+}
